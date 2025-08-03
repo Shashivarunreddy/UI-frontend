012d507d567f948d538f8bd216cad304b711118a
Shashivarunreddy <137184492+Shashivarunreddy@users.noreply.github.com>
2025-08-03 15:02:58 +0530

    added components

---
+++ src/fixed_components.tsx/header.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
+import { MainNav } from "@/components/main_nav"
+import { MobileNav } from "@/components/mobile_nav"
+import { Button } from "@/components/ui/button"
+import { Icons } from "@/components/icons"
+import { CommandMenu } from "@/components/command-menu"
+import { ModeSwitcher } from "@/components/mode-switcher"
+
+
+export function SiteHeader() {
+  return (
+    <header className="border-grid sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container-wrapper">
+        <div className="container flex h-14 items-center gap-2 md:gap-4">
+          <MainNav />
+          <MobileNav />
+          <div className="ml-auto flex items-center gap-2 md:flex-1 md:justify-end">
+            <div className="hidden w-full flex-1 md:flex md:w-auto md:flex-none">
+              <CommandMenu />
+            </div>
+            <nav className="flex items-center gap-0.5">
+              <Button
+                asChild
+                variant="ghost"
+                size="icon"
+                className="h-8 w-8 px-0"
+              >
+                <Link
+                  href={siteConfig.links.github}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Icons.gitHub className="h-4 w-4" />
+                  <span className="sr-only">GitHub</span>
+                </Link>
+              </Button>
+              <ModeSwitcher />
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
